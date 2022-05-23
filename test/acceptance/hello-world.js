1d4fa0350c32d1c2e5e5c43572b4ec15d5849849
sachin-kumar <sachinkumar@lambdatest.com>
2022-05-23 15:36:46 +0530

    update a flaky-test

--- test/acceptance/hello-world.js
+++ test/acceptance/hello-world.js
@@ -15,7 +15,7 @@ describe('hello-world', function () {
   describe('flaky-test', function () {
     it('sample-test', function () {
       var x = Math.random()
-      assert.equal(x, 1, "flaky test failed failed, expected: x=1");
+      assert.equal(x, 0, "flaky test failed failed, expected: x=0");
     })
   })
 
