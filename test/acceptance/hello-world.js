87e61954d2a88656fae65b57dbf581cbfb14fa4c
sachin-kumar <sachinkumar@lambdatest.com>
2022-05-23 12:52:13 +0530

    revert changes to hello-world.js

--- test/acceptance/hello-world.js
+++ test/acceptance/hello-world.js
@@ -11,33 +11,6 @@ describe('hello-world', function () {
     })
   })
 
-  describe('hello-world', function () {
-    describe('GET /', function () {
-      it('should respond with hello world', function (done) {
-        request(app)
-          .get('/')
-          .expect(200, 'Hello World', done)
-      })
-    })
-
-    describe('hello-world', function () {
-      describe('GET /', function () {
-        it('should respond with hello world', function (done) {
-          request(app)
-            .get('/')
-            .expect(200, 'Hello World', done)
-        })
-      })
-
-describe('hello-world', function () {
-  describe('GET /', function () {
-    it('should respond with hello world', function (done) {
-      request(app)
-        .get('/')
-        .expect(200, 'Hello World', done)
-    })
-  })
-
   describe('GET /missing', function () {
     it('should respond with 404', function (done) {
       request(app)
